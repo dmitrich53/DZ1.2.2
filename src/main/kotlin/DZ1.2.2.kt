4e5acfafa0871802994765a0c517b400bcd645ba
Ченцов Дмитрий <chdev@mail.ru>
2021-10-03 23:32:33 +0300

    Внесены исправления в файл DZ1.2.2

--- src/main/kotlin/DZ1.2.2.kt
+++ src/main/kotlin/DZ1.2.2.kt
@@ -1,16 +1,15 @@
 fun main() {
     while (true) {
         println("Введите количество положительных likes.")
-        var likes = readLine()
-        var d = likes!!.count()
-        var del = likes!!.removeRange(0, d - 1)
-        if (del != "1") {
+        val likes = readLine()
+        val numberLength = likes?.count()
+        val discardingPositions = likes?.removeRange(0, numberLength?.minus(1) ?: 0)
+        if (discardingPositions != "1") {
             println("Понравилось $likes людям.")
         } else {
             println("Понравилось $likes человеку.")
         }
         println("Новый ввод - ENTER. Завершить - введите q и ENTER.")
-        var prov = readLine()
-        if (prov == "q") break
+        if (readLine() == "q") break
     }
 }
